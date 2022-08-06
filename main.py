ac53666bbba7cfc97e03b45b9aa34b863b97b53a
root <root@Tejas.localdomain>
2022-08-06 16:31:04 +0530

    Graph Tested

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import math
 import streamlit as st
 from datetime import date
 
@@ -7,15 +8,16 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
+START = "2005-01-01"
+TODAY = "2016-01-01"
+#TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title("Stock Prediction App")
 
-stocks = ("AAPL","GOOG","MSFT","GME","TSLA","HDFCBANK.NS")
+stocks = ("SPY","AAPL","GOOG","MSFT","GME","TSLA","HDFCBANK.NS")
 selected_stock = st.selectbox("Select dataset for prediction",stocks)
 
-n_years = st.slider("Years of prediction:", 1, 5)
+n_years = st.slider("Months of prediction:", 2, 5)
 period = n_years * 365
 
 @st.cache
@@ -28,8 +30,8 @@ data_load_state = st.text("Load data...")
 data = load_data(selected_stock)
 data_load_state.text("Loading data...done!")
 
-st.subheader('Raw data')
-st.write(data.tail())
+# st.subheader('Raw data')
+# st.write(data.tail())
 
 def plot_raw_data():
     fig = go.Figure()
@@ -44,7 +46,7 @@ plot_raw_data()
 df_train = data[['Date','Close']]
 df_train = df_train.rename(columns={"Date": "ds", "Close":"y"})
 
-m = Prophet()
+m = Prophet()#changepoint_range=1,weekly_seasonality=True, changepoint_prior_scale=0.10)
 m.fit(df_train)
 future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
@@ -56,6 +58,18 @@ st.write('forecast data')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
-st.write('forecast components')
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+TODAY = "2018-04-01"
+data = yf.download(selected_stock, START, TODAY) #will return data in pandas data frame
+data.reset_index(inplace=True)
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'], name='original'))
+fig.add_trace(go.Scatter(x=forecast['ds'],y=(forecast['yhat_upper']+forecast['yhat_lower'])/2, name='predicted'))
+fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+st.plotly_chart(fig)
+
+# st.write('forecast components')
+# fig2 = m.plot_components(forecast)
+# st.write(fig2)
+
+#Testing
+st.write(forecast.tail())
